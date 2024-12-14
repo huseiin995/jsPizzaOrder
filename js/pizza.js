94eaf02cc6fabe9e0614ec85b136393ed265dd54
--replace-all <--replace-all>
2024-12-14 12:07:52 +0500

    first commit

---
+++ js/pizza.js
@@ -0,0 +1,44 @@
+const pizzaArray = [
+    {
+      id: 1,
+      name: "Pepperoni",
+      price: 2.23,
+      imgUrl: "../img/pizza1.png",
+    },
+    {
+      id: 2,
+      name: "Cheesy",
+      price: 5.99,
+      imgUrl: "../img/pizza2.jpg",
+    },
+    {
+      id: 3,
+      name: "Margarita",
+      price: 7.48,
+      imgUrl: "../img/pizza3.jpg",
+    },
+    {
+      id: 4,
+      name: "Hawaiian",
+      price: 9.32,
+      imgUrl: "../img/pizza4.jpg",
+    },
+    {
+      id: 5,
+      name: "Java",
+      price: 5,
+      imgUrl: "../img/pizza4.jpg",
+    },
+    {
+      id: 6,
+      name: "Javascript",
+      price: 10,
+      imgUrl: "../img/pizza4.jpg",
+    },
+    {
+      id: 7,
+      name: "Python",
+      price: 10,
+      imgUrl: "../img/pizza4.jpg",
+    },
+  ];
